741d23ec10c9b9c261027be77b2fde61dfbe82f6
Ebru Gulec <ebru@ebrugulec.com>
2019-01-20 10:44:40 +0300

    Updates

---
+++ precache-manifest.e76ce2c31edbdb3a9747a8224db756c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1860f61752df10964c4",
+    "url": "/responsive_mega_menu/static/js/runtime~main.b1860f61.js"
+  },
+  {
+    "revision": "e9f2d31c0aaeba4ef77e",
+    "url": "/responsive_mega_menu/static/js/main.e9f2d31c.chunk.js"
+  },
+  {
+    "revision": "35d4967322379881d30c",
+    "url": "/responsive_mega_menu/static/js/1.35d49673.chunk.js"
+  },
+  {
+    "revision": "e9f2d31c0aaeba4ef77e",
+    "url": "/responsive_mega_menu/static/css/main.dbb4d634.chunk.css"
+  },
+  {
+    "revision": "d74df2a4db4cd706a180149944e3c2ee",
+    "url": "/responsive_mega_menu/index.html"
+  }
+];
